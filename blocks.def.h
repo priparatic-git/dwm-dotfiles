9d017eb38b3a0b6b983c5b9983333ad0ea8b0c19
Aleksandr <kingkarl9old@gmail.com>
2024-07-16 23:13:27 +0500

    removed personal config file, and changed main config file

--- blocks.def.h
+++ blocks.def.h
@@ -1,9 +1,17 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
 
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+
+    /*{" " , "curl -s 'wttr.in/Almaty?format=%t'", 300, 0},*/
+
+    {" " , "date '+%A, %d.%m.%Y'", 60, 0},
+	{" ", "date '+%H:%M'", 1, 0},
+
+	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	5,		0},
+
+    {"Cpu:", "mpstat 1 1 | awk '/Average/ {print 100 - $NF}'", 5,  0},
+
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
